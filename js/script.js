3d73144f141eb69374155ae03ad2d4cb920075a5
justin69xd <bryanestuardo785@gmail.com>
2019-06-11 18:56:51 +0200

    perdon profe por hacerlo esperar por mi tarea pongame 10xd

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
-var p = angular.module("p"[])
-
-p.controller("pokemongt",function ($scope,$rootscope,$http) {
-	 }
-	// body...
-	$scope.listadopokemon = [];
-	$http({
-		method: "get";
-		url:"https://pokeapi.co/api/v2/pokemongt/459" 
-	}).then(function datos(x){
-	console.log(x); 
-	$scope.listadopokemon.push(x)
-})
+var pokemon= angular.module("pokemon",[])
+	pokemon.controller("listadopokemon",function($scope,$rootScope,$http){
+		$scope.Listado=[];
+		for(var i = 1; i<4000;i++){
+				$http({
+				method:"GET",
+				url:"https://pokeapi.co/api/v2/pokemon/"+i
+			}).then(function success(y){
+				$scope.Listado.push(y);
+				console.log("p.data.sprites.front_default")
+			})
+		}
+	})
+	
+	//https://pokeapi.co/api/v2/
